ae8bda5a2991e0ed68cfa145863e0872e8c9b7bd
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-09-18 15:48:08 +0200

    Complete refactor to increase performance and readability

--- src/index.ts
+++ src/index.ts
@@ -1,96 +1,67 @@
-export default function (input: string) {
-  const match = input.trim().match(re);
+type RegExpCaptureGroups = {
+  value: string;
+  [key: string]: string | undefined;
+};
+
+export default function parseBits(input: string) {
+  if (!input) {
+    throw new Error('Invalid input.');
+  }
+
+  const re = regex`
+    ^(?<value>\d*\.?\d+)
+    \s*
+    (?:
+      (?<decimal>
+        (?:[kmgtpez]b)|
+        (?:kilo|mega|giga|tera|peta|exa|zetta|yotta)bytes?)|
+      (?<binary>
+        (?:[kmgtpez]ib)|
+        (?:kibi|mebi|gibi|tebi|pebi|exbi|zebi|yobi)bytes?)|
+      (?:bytes?|b)
+    )?
+    $
+  `;
+
+  const match = input.trim().toLowerCase().match(re);
 
   if (!match) {
-    throw new Error(
-      'Malformed expression: invalid value or unit of measurement provided.'
-    );
+    throw new Error('Invalid input.');
   }
 
-  // If a string of integers was provided, return it as is
-  // e.g.: '123' -> 123
-  // e.g.: '123.95' -> 123
-  if (match[1] === input) {
-    return parseInt(input);
+  const { value, ...systems } = match.groups as RegExpCaptureGroups;
+  const magnitudes = ['k', 'm', 'g', 't', 'p', 'e', 'z', 'y'];
+  let exponent = 0;
+  let system = 1000;
+
+  for (const s in systems) {
+    const unit = systems[s];
+    if (!unit || unit.startsWith('b')) continue;
+    exponent = magnitudes.findIndex((m) => m === unit[0]) + 1;
+    system = s === 'binary' ? 1024 : 1000;
   }
 
-  const amount = _parseAmount(match);
-  const unit = _parseUnit(match);
-
-  const multiplier = _getMultiplier(unit);
-  const exponent = _getExponent(unit);
-
-  return amount * multiplier ** exponent;
+  return Math.floor(parseFloat(value) * system ** exponent);
 }
+// https://stackoverflow.com/questions/12317049/how-to-split-a-long-regular-expression-into-multiple-lines-in-javascript
+//build regexes without worrying about
+// - double-backslashing
+// - adding whitespace for readability
+// - adding in comments
+let clean = (piece: string) =>
+  piece
+    .replace(
+      /((^|\n)(?:[^\/\\]|\/[^*\/]|\\.)*?)\s*\/\*(?:[^*]|\*[^\/])*(\*\/|)/g,
+      '$1'
+    )
+    .replace(/((^|\n)(?:[^\/\\]|\/[^\/]|\\.)*?)\s*\/\/[^\n]*/g, '$1')
+    .replace(/\n\s*/g, '');
 
-function _parseAmount(match: RegExpMatchArray) {
-  return parseFloat(match[1]);
+function regex({ raw }: TemplateStringsArray, ...interpolations: string[]) {
+  return new RegExp(
+    interpolations.reduce(
+      (regex, insert, index) => regex + insert + clean(raw[index + 1]),
+      clean(raw[0])
+    )
+  );
 }
-
-function _parseUnit(match: RegExpMatchArray) {
-  // 4th capture group refers to the unit
-  // e.g.: bytes, kilobytes, megabytes...
-  return match[4].startsWith('byte')
-    ? match[4]
-    : match[4].toLowerCase().replace(/bytes?/, '');
-}
-
-function _getMultiplier(unit: string) {
-  const idx = units.findIndex((u) => u === unit);
-  const isDecimal = idx < decimalUnits.length;
-  return isDecimal ? 1000 : 1024;
-}
-
-function _getExponent(unit: string) {
-  const idx = units.findIndex((u) => u === unit);
-  return idx % (decimalUnits.length / 2);
-}
-
-const decimalUnits = [
-  'byte',
-  'kilo',
-  'mega',
-  'giga',
-  'tera',
-  'peta',
-  'exa',
-  'zetta',
-  'yotta',
-  'b',
-  'kb',
-  'mb',
-  'gb',
-  'tb',
-  'pb',
-  'eb',
-  'zb',
-  'yt',
-];
-
-const binaryUnits = [
-  'byte',
-  'kibi',
-  'mebi',
-  'gibi',
-  'tebi',
-  'pebi',
-  'exbi',
-  'zebi',
-  'yobi',
-  'b',
-  'kib',
-  'mib',
-  'gib',
-  'tib',
-  'pib',
-  'eib',
-  'zib',
-  'yit',
-];
-
-const units = decimalUnits.concat(binaryUnits);
-
-const re = new RegExp(
-  `^(\\d+(\\.\\d+)?)\\s?((${units.join('|')})(s|bytes?)?)?$`,
-  'i'
-);
